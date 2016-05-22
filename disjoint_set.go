b1a87b349b5f8f5065812cd69db73d0bf3898ef1
Rafael Rendón Pablo <rafaelrendonpablo@gmail.com>
2016-05-22 18:54:41 -0500

    Add docs to DisjointSet

--- disjoint_set.go
+++ disjoint_set.go
@@ -1,16 +1,20 @@
 package ds
 
+// DisjointSet  represents  a  data  structure  that  stores  information  about
+// connections in  a graph and  allows to determine  if two arbitrary  nodes are
+// connected in O(log n).
 type DisjointSet struct {
 	id   []int
 	size []int
 }
 
-func NewDisjointSet(nodes uint) *DisjointSet {
+// NewDisjointSet returns a new DisjointSet initialized to n elements.
+func NewDisjointSet(n uint) *DisjointSet {
 	ds := &DisjointSet{
-		id:   make([]int, nodes),
-		size: make([]int, nodes),
+		id:   make([]int, n),
+		size: make([]int, n),
 	}
-	for i := 0; i < int(nodes); i++ {
+	for i := 0; i < int(n); i++ {
 		ds.id[i] = i
 		ds.size[i] = 1
 	}
@@ -24,10 +28,12 @@ func (t *DisjointSet) root(u int) int {
 	return u
 }
 
+// Connected determines if nodes u and v are connected in some way.
 func (t *DisjointSet) Connected(u, v int) bool {
 	return t.root(u) == t.root(v)
 }
 
+// Connect connects nodes u and v.
 func (t *DisjointSet) Connect(u, v int) {
 	u = t.root(u)
 	v = t.root(v)
